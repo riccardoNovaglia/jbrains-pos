5482f8ffb79e9e228b57eb3717fc6421ffcd0d8c
novar <riccardo.novaglia@gmail.com>
2017-11-12 18:41:25 +0000

    Move some messages content from pos to display.

--- src/main/scala/pos/Display.scala
+++ src/main/scala/pos/Display.scala
@@ -1,5 +1,21 @@
 package pos
 
 abstract class Display {
-  def render(messageToDisplay: String): Unit
+  protected def render(messageToDisplay: String): Unit
+
+  def displayPrice(price: Float): Unit = {
+    render("$%.2f".format(price))
+  }
+
+  def displayEmptyBarcodeError(): Unit = {
+    render("Barcode read was empty")
+  }
+
+  def displayNoPriceFoundForItem(): Unit = {
+    render("No item was found for the barcode requested")
+  }
+
+  def displayErrorOccurredInPriceLookup(): Unit = {
+    render("An error occurred looking up the price for the barcode requested")
+  }
 }
--- src/main/scala/pos/PointOfSale.scala
+++ src/main/scala/pos/PointOfSale.scala
@@ -4,7 +4,7 @@ class PointOfSale(priceDisplay: Display, priceLookup: PriceLookup) {
 
   def onBarcode(barcode: String): Unit = {
     if (barcode.isEmpty) {
-      priceDisplay.render("Barcode read was empty")
+      priceDisplay.displayEmptyBarcodeError()
     } else {
       lookupAndDisplayPrice(barcode)
     }
@@ -13,10 +13,10 @@ class PointOfSale(priceDisplay: Display, priceLookup: PriceLookup) {
   private def lookupAndDisplayPrice(barcode: String): Unit = {
     try {
       val price = priceLookup.priceForBarcode(barcode)
-      priceDisplay.render("$%.2f".format(price))
+      priceDisplay.displayPrice(price)
     } catch {
-      case _: PriceNotFoundException => priceDisplay.render("No item was found for the barcode requested")
-      case _: Exception => priceDisplay.render("An error occurred looking up the price for the barcode requested")
+      case _: PriceNotFoundException  => priceDisplay.displayNoPriceFoundForItem()
+      case _: Exception               => priceDisplay.displayErrorOccurredInPriceLookup()
     }
   }
 }
